a43f14039b8c69152ec1fced9426b09481fc5e2c
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-05-09 15:28:18 +0700

    SAND-117 create database and schema for user

---
+++ lib/ReviewSite/Schema/Result/User.pm
@@ -0,0 +1,83 @@
+package ReviewSite::Schema::Result::User;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use Moose;
+use MooseX::NonMoose;
+use namespace::autoclean;
+extends 'DBIx::Class::Core';
+
+__PACKAGE__->load_components("InflateColumn::DateTime");
+
+=head1 NAME
+
+ReviewSite::Schema::Result::User
+
+=cut
+
+__PACKAGE__->table("users");
+
+=head1 ACCESSORS
+
+=head2 user_id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 user_name
+
+  data_type: 'varchar'
+  is_nullable: 1
+
+=head2 password
+
+  data_type: 'varchar'
+  is_nullable: 1
+
+=head2 first name
+
+  accessor: 'first_name'
+  data_type: 'varchar'
+  is_nullable: 1
+
+=head2 lastname
+
+  data_type: 'varchar'
+  is_nullable: 1
+
+=head2 email
+
+  data_type: 'varchar'
+  is_nullable: 1
+
+=cut
+
+__PACKAGE__->add_columns(
+  "user_id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "user_name",
+  { data_type => "varchar", is_nullable => 1 },
+  "password",
+  { data_type => "varchar", is_nullable => 1 },
+  "first name",
+  { accessor => "first_name", data_type => "varchar", is_nullable => 1 },
+  "lastname",
+  { data_type => "varchar", is_nullable => 1 },
+  "email",
+  { data_type => "varchar", is_nullable => 1 },
+);
+__PACKAGE__->set_primary_key("user_id");
+#set relationship later
+
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2011-05-09 15:08:42
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:L3Wzq6T/3kXD155GIyJS+g
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+__PACKAGE__->meta->make_immutable;
+1;
---
+++ t/model_DB.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::More;
+
+BEGIN { use_ok 'ReviewSite::Model::DB' }
+
+done_testing();
