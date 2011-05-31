4e4ab1436e412d19367dd7e1eab39f9b9038b5f9
Nudjaree Chanprasert <jew@abctech-thailand.com>
2011-05-31 16:09:20 +0700

    SAND118 modified ResultSet/Place.pm using +select and +as

--- lib/ReviewSite/Schema/ResultSet/Place.pm
+++ lib/ReviewSite/Schema/ResultSet/Place.pm
@@ -11,9 +11,9 @@ sub top3 {
 	my $self = shift;
 	return $self->search( { },
 	 {  join     =>    'review',
-	 	select   => [ 'location','long','la','placename','place_id',{ avg => 'rate' },{count => 'review.review_id' } ],
-	    as       => [qw/location la long placename place_id avgrate count_review/],
-	 	group_by => [qw/me.place_id/], 
+	 	'+select'   => [ { avg => 'rate' },{ count => 'review.review_id' } ],
+	    '+as'       => [ qw/avgrate count_review/ ],
+	 	group_by => [ qw/me.place_id/ ], 
 	 	order_by => { -desc => 'rate'},
 	 	rows => 3 
 
